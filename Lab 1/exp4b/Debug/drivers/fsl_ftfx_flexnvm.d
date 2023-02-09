449fd15eca3b28332498823be00bd5679f0e3a4a
Jingming Liu <liuj171@ece.mcmaster.ca>
2023-02-09 01:39:44 -0500

    moved exp4b from another dir

---
+++ Lab 1/exp4b/Debug/drivers/fsl_ftfx_flexnvm.d
@@ -0,0 +1,30 @@
+drivers/fsl_ftfx_flexnvm.o drivers/fsl_ftfx_flexnvm.d: \
+ ../drivers/fsl_ftfx_flexnvm.c ../drivers/fsl_ftfx_flexnvm.h \
+ ../drivers/fsl_ftfx_controller.h ../drivers/fsl_ftfx_features.h \
+ ../drivers/fsl_common.h \
+ C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\device/fsl_device_registers.h \
+ C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\device/MK66F18.h \
+ C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\CMSIS/core_cm4.h \
+ C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\CMSIS/cmsis_version.h \
+ C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\CMSIS/cmsis_compiler.h \
+ C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\CMSIS/cmsis_gcc.h \
+ C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\device/system_MK66F18.h \
+ C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\device/MK66F18_features.h \
+ ../drivers/fsl_common_arm.h ../drivers/fsl_clock.h \
+ ../drivers/fsl_ftfx_adapter.h ../drivers/fsl_ftfx_utilities.h
+../drivers/fsl_ftfx_flexnvm.h:
+../drivers/fsl_ftfx_controller.h:
+../drivers/fsl_ftfx_features.h:
+../drivers/fsl_common.h:
+C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\device/fsl_device_registers.h:
+C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\device/MK66F18.h:
+C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\CMSIS/core_cm4.h:
+C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\CMSIS/cmsis_version.h:
+C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\CMSIS/cmsis_compiler.h:
+C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\CMSIS/cmsis_gcc.h:
+C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\device/system_MK66F18.h:
+C:\Users\guoj69\Documents\COMPENG4DS4\LAB\ 1\frdmk66f_bubble_peripheral\device/MK66F18_features.h:
+../drivers/fsl_common_arm.h:
+../drivers/fsl_clock.h:
+../drivers/fsl_ftfx_adapter.h:
+../drivers/fsl_ftfx_utilities.h:
