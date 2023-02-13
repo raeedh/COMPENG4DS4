014276122d3e548bce67cd4d3a7f5f854a72b5b6
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2023-02-13 15:48:35 -0500

    clean up exp2a/p2

--- Lab 2/experiment2a/source/freertos_hello.c
+++ Lab 2/experiment2a/source/freertos_hello.c
@@ -25,12 +25,11 @@
  * Definitions
  ******************************************************************************/
 
-
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-void producer_queue(void* pvParameters);
-void consumer_queue(void* pvParameters);
+void producer_queue(void *pvParameters);
+void consumer_queue(void *pvParameters);
 
 /*******************************************************************************
  * Code
@@ -38,65 +37,71 @@ void consumer_queue(void* pvParameters);
 
 int main(void)
 {
-BaseType_t status;
-/* Init board hardware. */
-BOARD_InitBootPins();
-BOARD_InitBootClocks();
-BOARD_InitDebugConsole();
-QueueHandle_t queue1 = xQueueCreate(1, sizeof(int));
-if (queue1 == NULL)
-{
-PRINTF("Queue creation failed!.\r\n");
-while (1);
-}
-status = xTaskCreate(producer_queue, "producer", 200, (void*)queue1, 2, NULL);
-if (status != pdPASS)
-{
-PRINTF("Task creation failed!.\r\n");
-while (1);
-}
-status = xTaskCreate(consumer_queue, "consumer", 200, (void*)queue1, 2, NULL);
-if (status != pdPASS)
-{
-PRINTF("Task creation failed!.\r\n");
-while (1);
-}
-vTaskStartScheduler();
-while (1)
-{}
+    BaseType_t status;
+    /* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitDebugConsole();
+
+    QueueHandle_t queue1 = xQueueCreate(1, sizeof(int));
+    if (queue1 == NULL)
+    {
+        PRINTF("Queue creation failed!.\r\n");
+        while (1);
+    }
+
+    status = xTaskCreate(producer_queue, "producer", 200, (void *)queue1, 2, NULL);
+    if (status != pdPASS)
+    {
+        PRINTF("Task creation failed!.\r\n");
+        while (1);
+    }
+
+    status = xTaskCreate(consumer_queue, "consumer", 200, (void *)queue1, 2, NULL);
+    if (status != pdPASS)
+    {
+        PRINTF("Task creation failed!.\r\n");
+        while (1);
+    }
+
+    vTaskStartScheduler();
+    while (1)
+    {}
 }
 
-void producer_queue(void* pvParameters)
+void producer_queue(void *pvParameters)
 {
-QueueHandle_t queue1 = (QueueHandle_t)pvParameters;
-BaseType_t status;
-int counter = 0;
-while(1)
-{
-counter++;
-status = xQueueSendToBack(queue1, (void*) &counter, portMAX_DELAY);
-if (status != pdPASS)
-{
-PRINTF("Queue Send failed!.\r\n");
-while (1);
-}
-vTaskDelay(1000 / portTICK_PERIOD_MS);
-}
+    QueueHandle_t queue1 = (QueueHandle_t)pvParameters;
+    BaseType_t status;
+    int counter = 0;
+
+    while (1)
+    {
+        counter++;
+        status = xQueueSendToBack(queue1, (void *)&counter, portMAX_DELAY);
+        if (status != pdPASS)
+        {
+            PRINTF("Queue Send failed!.\r\n");
+            while (1);
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
-void consumer_queue(void* pvParameters)
+void consumer_queue(void *pvParameters)
 {
-QueueHandle_t queue1 = (QueueHandle_t)pvParameters;
-BaseType_t status;
-int received_counter;
-while(1)
-{
-status = xQueueReceive(queue1, (void *) &received_counter, portMAX_DELAY);
-if (status != pdPASS)
-{
-PRINTF("Queue Receive failed!.\r\n");
-while (1);
-}
-PRINTF("Received Value = %d\r\n", received_counter);
-}
+    QueueHandle_t queue1 = (QueueHandle_t)pvParameters;
+    BaseType_t status;
+    int received_counter;
+
+    while (1)
+    {
+        status = xQueueReceive(queue1, (void *)&received_counter, portMAX_DELAY);
+        if (status != pdPASS)
+        {
+            PRINTF("Queue Receive failed!.\r\n");
+            while (1);
+        }
+        PRINTF("Received Value = %d\r\n", received_counter);
+    }
 }
