41d106e884ea2b739b36df3af9969d793de96759
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2023-02-13 16:20:56 -0500

    cleanup exp4

--- Lab 2/experiment4/source/freertos_hello.c
+++ Lab 2/experiment4/source/freertos_hello.c
@@ -28,75 +28,79 @@
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-static void hello_task(void *pvParameters);
+void hello_task(void *pvParameters);
+void timerCallbackFunction(TimerHandle_t timer_handle);
+void timerCallbackFunction2(TimerHandle_t timer_handle);
 
 /*******************************************************************************
  * Code
  ******************************************************************************/
-/*!
- * @brief Application entry point.
- */
 int main(void)
 {
-BaseType_t status;
-/* Init board hardware. */
-BOARD_InitBootPins();
-BOARD_InitBootClocks();
-BOARD_InitDebugConsole();
-status = xTaskCreate(hello_task, "Hello_task", 200, NULL, 2, NULL);
-if (status != pdPASS)
-{
-PRINTF("Task creation failed!.\r\n");
-while (1);
-}
-TimerHandle_t timer_handle = xTimerCreate("Single Shot timer",
-5000 / portTICK_PERIOD_MS,
-pdFALSE,
-NULL,
-timerCallbackFunction);
-status = xTimerStart(timer_handle, 0);
-if(status != pdPASS)
-{
-PRINTF("Couldn’t start the timer!.\r\n");
-while (1);
-}
-TimerHandle_t timer_handle2 = xTimerCreate("Periodic timer",
-1000 / portTICK_PERIOD_MS,
-pdTRUE,
-NULL,
-timerCallbackFunction2);
-status = xTimerStart(timer_handle2, 0);
-if(status != pdPASS)
-{
-PRINTF("Couldn’t start the timer!.\r\n");
-while (1);
-}
-vTaskStartScheduler();
-while (1)
-{}
+    BaseType_t status;
+    /* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitDebugConsole();
+
+    status = xTaskCreate(hello_task, "Hello_task", 200, NULL, 2, NULL);
+    if (status != pdPASS)
+    {
+        PRINTF("Task creation failed!.\r\n");
+        while (1);
+    }
+
+    TimerHandle_t timer_handle = xTimerCreate("Single Shot timer",
+                                              5000 / portTICK_PERIOD_MS,
+                                              pdFALSE,
+                                              NULL,
+                                              timerCallbackFunction);
+
+    status = xTimerStart(timer_handle, 0);
+    if (status != pdPASS)
+    {
+        PRINTF("Couldn't start the timer!.\r\n");
+        while (1);
+    }
+
+    TimerHandle_t timer_handle2 = xTimerCreate("Periodic timer",
+                                               1000 / portTICK_PERIOD_MS,
+                                               pdTRUE,
+                                               NULL,
+                                               timerCallbackFunction2);
+
+    status = xTimerStart(timer_handle2, 0);
+    if (status != pdPASS)
+    {
+        PRINTF("Couldn't start the timer!.\r\n");
+        while (1);
+    }
+
+    vTaskStartScheduler();
+    while (1)
+    {}
 }
 
-/*!
- * @brief Task responsible for printing of "Hello world." message.
- */
-static void hello_task(void *pvParameters)
+void hello_task(void *pvParameters)
 {
-    for (;;)
+    while (1)
     {
-        PRINTF("Hello world.\r\n");
-        vTaskSuspend(NULL);
+        PRINTF("Hello World\r\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
 void timerCallbackFunction(TimerHandle_t timer_handle)
 {
-PRINTF("Hello from the single shot timer callback.\r\n");
+    PRINTF("Hello from the single shot timer callback.\r\n");
 }
+
 void timerCallbackFunction2(TimerHandle_t timer_handle)
 {
-static int counter = 0;
-PRINTF("Hello from the periodic timer callback. Counter = %d\r\n", counter);
-counter++;
-if(counter >= 10)
-xTimerStop(timer_handle, 0);
+    static int counter = 0;
+
+    PRINTF("Hello from the periodic timer callback. Counter = %d\r\n", counter);
+    counter++;
+    if (counter >= 10)
+        xTimerStop(timer_handle, 0);
 }
