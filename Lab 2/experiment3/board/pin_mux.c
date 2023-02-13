12de6e20ae1103af5a03ce7a242973d7ce738ae8
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2023-02-13 16:16:11 -0500

    clean up exp3

--- Lab 2/experiment3/board/pin_mux.c
+++ Lab 2/experiment3/board/pin_mux.c
@@ -59,21 +59,11 @@ BOARD_InitPins:
  * END ****************************************************************************************************************/
 void BOARD_InitPins(void)
 {
-    /* Port B Clock Gate Control: Clock enabled */
-    CLOCK_EnableClock(kCLOCK_PortB);
-
-    /* PORTB16 (pin E10) is configured as UART0_RX */
-    PORT_SetPinMux(PORTB, 16U, kPORT_MuxAlt3);
-
-    /* PORTB17 (pin E9) is configured as UART0_TX */
-    PORT_SetPinMux(PORTB, 17U, kPORT_MuxAlt3);
-
-    SIM->SOPT5 = ((SIM->SOPT5 &
-                   /* Mask bits to zero which are setting */
-                   (~(SIM_SOPT5_UART0TXSRC_MASK)))
-
-                  /* UART 0 transmit data source select: UART0_TX pin. */
-                  | SIM_SOPT5_UART0TXSRC(SOPT5_UART0TXSRC_UART_TX));
+	CLOCK_EnableClock(kCLOCK_PortC);
+		PORT_SetPinMux(PORTC, 12U, kPORT_MuxAlt3);
+		PORT_SetPinMux(PORTC, 13U, kPORT_MuxAlt3);
+		PORT_SetPinMux(PORTC, 14U, kPORT_MuxAlt3);
+		PORT_SetPinMux(PORTC, 15U, kPORT_MuxAlt3);
 }
 /***********************************************************************************************************************
  * EOF
